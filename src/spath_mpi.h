524297fb826d43ca9557ebfbf59418d1bf43a684
Elsa Gonsiorowski (Uranus) <gonsiorowski1@llnl.gov>
2019-09-09 17:17:50 -0700

    doxygen-style comments

--- src/spath_mpi.h
+++ src/spath_mpi.h
@@ -5,13 +5,17 @@
 
 #include "spath.h"
 
+/** \file spath.h
+ *  \ingroup spath
+ *  \brief functions to send/recv an spath object */
+
 /* enable C++ codes to include this header directly */
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #if 0
-/* send/recv path, recv_path should be from spath_new() */
+/** send/recv path, recv_path should be from spath_new() */
 int spath_sendrecv(
   const spath* send_path,
   int send_rank,
@@ -21,7 +25,7 @@ int spath_sendrecv(
 );
 #endif
 
-/* broadcast path, path should be from spath_new() on non-roots */
+/** broadcast path, path should be from spath_new() on non-roots */
 int spath_bcast(spath* path, int root, MPI_Comm comm);
 
 /* enable C++ codes to include this header directly */
--- src/spath_util.h
+++ src/spath_util.h
@@ -1,7 +1,12 @@
 #include  <stdlib.h>
 #include  <stdio.h>
-/* allocate size bytes, returns NULL if size == 0,
- *  * calls er_abort if allocation fails */
-#define SPATH_MALLOC(X) spath_malloc(X, __FILE__, __LINE__);
-void* spath_malloc(size_t size, const char* file, int line);
 
+/** \file spath_util.h
+ *  \ingroup spath
+ *  \brief utilities for spath */
+
+#define SPATH_MALLOC(X) spath_malloc(X, __FILE__, __LINE__);
+
+/** allocate size bytes, returns NULL if size == 0 */
+/* TODO: should somehowe abort if allocation fails */
+void* spath_malloc(size_t size, const char* file, int line);
