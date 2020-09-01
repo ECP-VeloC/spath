602ac1c9f5da087059e569865fa4826b1d5a2a2d
Gregory A. Kosinovsky <kosinov@quartz386.llnl.gov>
2020-09-01 16:27:42 -0700

    REALLY fixed cmake/SPATH_ADD_TEST.cmake

--- cmake/SPATH_ADD_TEST.cmake
+++ cmake/SPATH_ADD_TEST.cmake
@@ -1,10 +1,10 @@
 function(SPATH_ADD_TEST name args outputs)
 
   # job launcher
-  if(NOT DEFINED VELOC_RESOURCE_MANAGER)
-    set(test_param srun -N 3 -n 3)
+  if(NOT(${VELOC_RESOURCE_MANAGER}))
+    set(test_param mpirun -np 3)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "NONE")
-    set(test_param mpirun -np 1)
+    set(test_param mpirun -np 3)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "LSF")
     set(test_param jsrun -r 1)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "SLURM")
