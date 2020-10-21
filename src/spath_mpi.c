2eb4376fe46422c51237e8d7c6846cf5c1dcd341
Gregory Kosinovsky <kosinovsky1@llnl.gov>
2020-10-21 13:19:37 -0700

    Update src/spath_mpi.c
    Co-authored-by: Adam Moody <moody20@llnl.gov>

--- src/spath_mpi.c
+++ src/spath_mpi.c
@@ -23,7 +23,6 @@ Functions to send/recv paths with MPI
  * receivers must pass in a newly allocated path from spath_new() */
 int spath_bcast(spath* path, int root, MPI_Comm comm)
 {
-  int need_to_abort = 0;
   /* if pointer is NULL, throw an error and flag a need to abort*/
   if (path == NULL) {
     fprintf(stderr, "NULL pointer passed for path @ %s:%d\n",
