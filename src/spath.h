2941057c53a942b0d44013ce7bd1346aa24b3539
Elsa Gonsiorowski, PhD <gonsiorowski1@llnl.gov>
2021-03-24 10:11:09 -0700

    Incremental release v0.0.2

--- src/spath.h
+++ src/spath.h
@@ -12,7 +12,7 @@ extern "C" {
 #define SPATH_SUCCESS (0)
 #define SPATH_FAILURE (1)
 
-#define SPATH_VERSION "0.0.1"
+#define SPATH_VERSION "0.0.2"
 
 /* TODO: for formatted strings, use special %| character (or something
  * similar) to denote directories in portable way */
